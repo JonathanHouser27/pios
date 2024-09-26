27eda514924e571e18dc3535a9ba1edd991edcf4
JonathanHouser27 <jonhouser95@gmail.com>
2024-09-25 22:34:14 -0500

    added function to fetch and print the execution level

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,22 +1,40 @@
+#include "rprintf.h"
+#include "rprintf.c"
+#include "serial.c"
+
 char glbl[128];
 
 char huge_array[8192];
 
+unsigned int el;
+
+unsigned int getEL(){
+  asm("mrs %0, CurrentEL"
+    :"=r"(el)
+    :
+    :);
+  return el>>2;
+}
+
+void print_execution_level() {
+  el = getEL();
+  esp_printf(putc, "Current Execution Level is %d\r\n", el);
+}
 
 // function to retrieve the counter
 unsigned long get_timer_count() {
-
+  // point to the counter in memory
   unsigned long *timer_count_register = 0x3f003004;
-
-  return *timer_count_register;
+  return *timer_count_register; // return the count
 }
 
 
 // function to wait for 1ms
 void wait_1ms() {
-
+  // set the start time to the count from memory
   unsigned long start_time = get_timer_count();
-
+  // call the count from memory and once that - start_time < 1000
+  // then 1ms has elapsed, end the loop and continue the program
   while ((get_timer_count() - start_time) < 1000) {
 
   }
@@ -26,10 +44,11 @@ void wait_1ms() {
 // function to call for the timer method, wait 1ms, and
 void kernel_main() {
 
+  print_execution_level();
+
+
   unsigned long timer_value = get_timer_count();
-
   wait_1ms();
-
   timer_value = get_timer_count();
 
   extern int __bss_start, __bss_end;
