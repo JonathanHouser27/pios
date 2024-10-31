4b4d139f050948962191a614411196ebc830d012
JonathanHouser27 <jonhouser95@gmail.com>
2024-10-31 10:58:57 -0500

    moved wait into delays.h and delays.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,7 +1,9 @@
 #include "rprintf.h"
+#include "rprintf.c"
 #include "serial.h"
 #include "mmu.h"
 #include "list.h"
+#include "delays.h"
 
 char glbl[128];
 
@@ -22,43 +24,26 @@ void print_execution_level() {
   esp_printf(putc, "Current Execution Level is %d\r\n", el);
 }
 
-// function to retrieve the counter
-unsigned long get_timer_count() {
-  // point to the counter in memory
-  unsigned long *timer_count_register = 0x3f003004;
-  return *timer_count_register; // return the count
-}
+// MOVED TO DELAYS.H
 
-
-// function to wait for 1ms
-void wait_1ms() {
-  // set the start time to the count from memory
-  unsigned long start_time = get_timer_count();
-  // call the count from memory and once that - start_time < 1000
-  // then 1ms has elapsed, end the loop and continue the program
-  while ((get_timer_count() - start_time) < 1000) {
-
-  }
-}
-
-
-// function to call for the timer method, wait 1ms, and
 void kernel_main() {
 
-  print_execution_level();
+//  fatInit();
+//  fatOpen("file_name", struct rde *r);
+//  fatRead(char* buf, int n, struct rde *r);
 
-  unsigned long timer_value = get_timer_count();
-  wait_1ms();
-  timer_value = get_timer_count();
+  print_execution_level();
+  // call to the delays file with the wait_msec fuction
+  wait_msec(5);
 
   extern int __bss_start, __bss_end;
-  char *bssstart, *bssend, *i;
+  char *bssstart, *bssend;
 
-  bssstart = &__bss_start;
-  bssend = &__bss_end;
+  bssstart = (char *)&__bss_start;
+  bssend = (char *)&__bss_end;
 
   // for loop to go throughout the bss and change the values to 0
-  for(*i = *bssstart; *i <= *bssend; i++){
+  for(char *i = bssstart; i <= bssend; i++){
    *i = 0;
   }
 
